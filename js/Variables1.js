e18c54b07ba5aa59b652f8c07420548822eb3d79
apolo4427 <top152695@gmail.com>
2024-05-01 22:52:25 -0500

    primer commit

---
+++ js/Variables1.js
@@ -0,0 +1,35 @@
+//alert("hola manito");
+"use strict";
+
+//numerico
+var numero1;//declaracion de variable
+let numero2;
+
+numero1 = "2";
+
+numero2 = "13";
+
+var numero3= numero1+numero2;
+
+//alert(typeof(numero3));
+
+// if(Number(numero2)){
+// //     let manito="definicion de let";//variable local para el bloque en el que se define
+
+// //     var manito2="definicion de var";//es global
+
+//     // let manito = prompt("Dime el nombre del manito: ");
+
+//     // alert(manito);
+
+// }
+//alert(manito);
+//Number(numero1) devuelve true si el argumento "numero1" en este caso, puede ser un numero.
+//alert(Number(numero3)*2);
+
+
+
+
+
+
+
---
+++ js/colecciones2.js
@@ -0,0 +1,6 @@
+let unPelao = "Sergio";
+
+let losPelaos = ["Daniel", "Santiago", unPelao, 3, true];
+
+//console.log("hola mundo");
+console.log(losPelaos[0]);
